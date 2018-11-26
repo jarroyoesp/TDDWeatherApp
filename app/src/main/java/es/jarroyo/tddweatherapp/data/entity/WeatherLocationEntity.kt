6a32a777d48211a38fed0b448f9ea588ce369a22
Javier Arroyo <javier.arroyo@microhealth.com>
2018-11-26 15:38:32 +0100

    Mapper in class

--- app/src/main/java/es/jarroyo/tddweatherapp/data/entity/WeatherLocationEntity.kt
+++ app/src/main/java/es/jarroyo/tddweatherapp/data/entity/WeatherLocationEntity.kt
@@ -2,6 +2,10 @@ package es.jarroyo.tddweatherapp.data.entity
 
 import android.arch.persistence.room.Entity
 import android.arch.persistence.room.PrimaryKey
+import es.jarroyo.tddweatherapp.domain.model.location.WeatherLocation
+
+fun WeatherLocationEntity.toDomain (): WeatherLocation = WeatherLocation(id, cityName)
+
 @Entity(tableName = "WEATHER_LOCATION")
 class WeatherLocationEntity(
         @PrimaryKey(autoGenerate = true)
--- app/src/main/java/es/jarroyo/tddweatherapp/domain/model/location/WeatherLocation.kt
+++ app/src/main/java/es/jarroyo/tddweatherapp/domain/model/location/WeatherLocation.kt
@@ -1,5 +1,9 @@
 package es.jarroyo.tddweatherapp.domain.model.location
 
-class WeatherLocation(var id: Long = 0,
-                      var cityName: String) {
+import es.jarroyo.tddweatherapp.data.entity.WeatherLocationEntity
+
+open data class WeatherLocation(var id: Long = 0,
+                                var cityName: String = "") {
 }
+
+fun WeatherLocation.toEntity (): WeatherLocationEntity = WeatherLocationEntity(id, cityName)
